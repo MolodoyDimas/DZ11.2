c5943a0235c10dc21a4cb30b2c23987a7b49b77b
Dmitry <kip@gmail.com>
2023-07-25 16:54:30 +0700

    Добавили функцию Делает заглавными первые буквы каждого слова в строке

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper1(text):
+    '''Переводит текст в верхний регистр!'''
     return text.upper()
 
+
+def capital_letters(text):
+    '''Делает заглавными первые буквы каждого слова в строке'''
+    return text.title()
